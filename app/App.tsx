47523d546d1643d61304d9474f3f301502aaba20
!Mensch <assassinste@gmail.com>
2023-03-24 07:55:02 +0800

    refacotr: Two answer box for future references (zh and en)

--- app/App.tsx
+++ app/App.tsx
@@ -1,19 +1,19 @@
-"use client";
-import { genetrateCopyButton } from "@/utils/helper";
-import createLiveChatCompletion from "@/utils/liveGptClient";
-import hljs from "highlight.js";
-import { marked } from "marked";
-import { SetStateAction, useEffect, useRef, useState } from "react";
+'use client';
+import { genetrateCopyButton } from '@/utils/helper';
+import createLiveChatCompletion from '@/utils/liveGptClient';
+import hljs from 'highlight.js';
+import { marked } from 'marked';
+import { SetStateAction, useEffect, useRef, useState } from 'react';
 
 marked.setOptions({
-  langPrefix: "hljs language-",
+  langPrefix: 'hljs language-',
   highlight: function (code: any, lang: string) {
     return hljs.highlightAuto(code, [
-      "html",
-      "javascript",
-      "python",
-      "rust",
-      "go",
+      'html',
+      'javascript',
+      'python',
+      'rust',
+      'go',
     ]).value;
   },
 });
@@ -25,10 +25,12 @@ export default function App({
   parseHTML?: boolean;
   defaultDirection?: string;
 }) {
-  const [apiKey, setApiKey] = useState("");
-  const [maxTokens, setMaxTokens] = useState("2048");
+  const [apiKey, setApiKey] = useState('');
+  const [maxTokens, setMaxTokens] = useState('2048');
   const [direction, setDirection] = useState(
-  defaultDirection ? defaultDirection : `From now on, I will give you code snippets, then
+    defaultDirection
+      ? defaultDirection
+      : `From now on, I will give you code snippets, then
 - paragraph 1: explain what it does in short yet concise technical explanation
 - paragraph 2: same thing but using real-world analogy in happy tones with emojis
 
@@ -37,57 +39,57 @@ Also
 - Between paragraph, there shall be two <br>s
 - Answer in Simplified Chinese (be native)
 
-Do not start unless I say so`
-);
+Do not start unless I say so`,
+  );
   const [question, setQuestion] = useState(`def main():
     pass
   `);
-  const [answer, setAnswer] = useState("...");
+  const [answer, setAnswer] = useState('...');
 
   const [isLoading, setIsLoading] = useState(false);
 
-  const resultRef = useRef("");
+  const resultRef = useRef('');
 
-  const tailRef = useRef("");
+  const tailRef = useRef('');
 
   const storeApiKey = (e: { target: { value: SetStateAction<string> } }) => {
     setApiKey(e.target.value);
-    localStorage.setItem("apiKey", String(e.target.value));
+    localStorage.setItem('apiKey', String(e.target.value));
   };
 
   const handleSubmitPromptBtnClicked = () => {
-    if (question !== "" && !isLoading) {
+    if (question !== '' && !isLoading) {
       setIsLoading(true);
-      setAnswer("");
+      setAnswer('');
 
       const source = createLiveChatCompletion(
         apiKey,
         Number(maxTokens),
         direction,
-        question
+        question,
       );
 
-      source.addEventListener("message", (e: { data: string }) => {
-        if (e.data != "[DONE]") {
+      source.addEventListener('message', (e: { data: string }) => {
+        if (e.data != '[DONE]') {
           const payload = JSON.parse(e.data);
           if (
             Object.prototype.hasOwnProperty.call(
               payload.choices[0].delta,
-              "content"
+              'content',
             )
           ) {
             const text = payload.choices[0].delta.content;
 
-            if (text.includes("```")) {
-              if (tailRef.current === "") {
-                tailRef.current = "\n```";
+            if (text.includes('```')) {
+              if (tailRef.current === '') {
+                tailRef.current = '\n```';
               } else {
-                tailRef.current = "";
+                tailRef.current = '';
               }
             }
 
-            if (text === "`") {
-              tailRef.current = "";
+            if (text === '`') {
+              tailRef.current = '';
             }
 
             resultRef.current = resultRef.current + text;
@@ -100,27 +102,27 @@ Do not start unless I say so`
       });
 
       source.addEventListener(
-        "readystatechange",
+        'readystatechange',
         (e: { readyState: number }) => {
           if (e.readyState >= 2) {
-            tailRef.current = "";
+            tailRef.current = '';
             setIsLoading(false);
             setTimeout(() => {
               genetrateCopyButton();
             }, 500);
           }
-        }
+        },
       );
 
       source.stream();
     } else {
-      alert("Please insert a prompt!");
+      alert('Please insert a prompt!');
     }
   };
 
   useEffect(() => {
     // Perform localStorage action
-    const localKey = localStorage.getItem("apiKey");
+    const localKey = localStorage.getItem('apiKey');
     if (localKey) {
       setApiKey(localKey);
     }
@@ -179,7 +181,7 @@ Do not start unless I say so`
               className="resize-vertical h-12 w-full px-5 py-2 font-medium border border-b-4 border-r-4 border-black rounded-lg shadow-lg hover:shadow-sm"
               name="system"
               value={direction}
-              onChange={(e) => setDirection(e.target.value)}
+              onChange={e => setDirection(e.target.value)}
             />
           </label>
         </div>
@@ -192,7 +194,7 @@ Do not start unless I say so`
               className="resize-none font-mono h-56 xl:h-24 w-full px-5 py-2 font-medium border border-b-4 border-r-4 border-black rounded-lg shadow-lg hover:shadow-sm"
               name="user"
               value={question}
-              onChange={(e) => setQuestion(e.target.value)}
+              onChange={e => setQuestion(e.target.value)}
             />
           </label>
         </div>
@@ -201,8 +203,8 @@ Do not start unless I say so`
             disabled={isLoading}
             className={
               isLoading
-                ? "w-full px-6 py-2 my-2 text-gray-700 border border-b-4 border-r-4 border-gray-500 hover:bg-gray-500 hover:text-gray-100 rounded-lg shadow-lg"
-                : "w-full px-6 py-2 my-2 text-indigo-700 border border-b-4 border-r-4 border-indigo-500 hover:bg-indigo-500 hover:text-indigo-100 rounded-lg shadow-lg"
+                ? 'w-full px-6 py-2 my-2 text-gray-700 border border-b-4 border-r-4 border-gray-500 hover:bg-gray-500 hover:text-gray-100 rounded-lg shadow-lg'
+                : 'w-full px-6 py-2 my-2 text-indigo-700 border border-b-4 border-r-4 border-indigo-500 hover:bg-indigo-500 hover:text-indigo-100 rounded-lg shadow-lg'
             }
             onClick={handleSubmitPromptBtnClicked}
           >
@@ -214,17 +216,31 @@ Do not start unless I say so`
             <span className="text-xs font-semibold inline-block py-1 px-2 my-2 uppercase rounded text-slate-600 bg-slate-200 last:mr-0 mr-1">
               Assistant
             </span>
-            <div className="overflow-auto h-56 xl:h-96 w-full xl:w-2/4 px-5 py-2 font-medium border border-b-4 border-r-4 border-black rounded-lg shadow-lg hover:shadow-sm">
-              {parseHTML ? (
-                <div
-                  className="flex flex-col"
-                  dangerouslySetInnerHTML={{
-                    __html: marked.parse(answer + tailRef.current),
-                  }}
-                />
-              ) : (
-                <div className="flex flex-col">{answer}</div>
-              )}
+            <div className="flex flex-col xl:flex-row gap-4">
+              <div className="overflow-auto h-56 xl:h-96 w-full xl:w-2/4 px-5 py-2 font-medium border border-b-4 border-r-4 border-black rounded-lg shadow-lg hover:shadow-sm">
+                {parseHTML ? (
+                  <div
+                    className="flex flex-col"
+                    dangerouslySetInnerHTML={{
+                      __html: marked.parse(answer + tailRef.current),
+                    }}
+                  />
+                ) : (
+                  <div className="flex flex-col">{answer}</div>
+                )}
+              </div>
+              <div className="overflow-auto h-56 xl:h-96 xl:w-2/4 px-5 py-2 font-medium border border-b-4 border-r-4 border-black rounded-lg shadow-lg hover:shadow-sm">
+                {parseHTML ? (
+                  <div
+                    className="flex flex-col"
+                    dangerouslySetInnerHTML={{
+                      __html: marked.parse(answer + tailRef.current),
+                    }}
+                  />
+                ) : (
+                  <div className="flex flex-col">{answer}</div>
+                )}
+              </div>
             </div>
           </label>
         </div>
